9a4020a5b05380c94c6541e36cf37bb9c0316f64
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-04-19 01:01:49 +0330

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,22 +1,31 @@
 from django.contrib import admin
+from .models import Product,Category
+# Register your models here.
 
-from .models import Category, Product
-
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name','slug']
+    list_display_links = ['name']
+    list_editable = ['slug']
+    list_filter = ['name']
+    list_per_page = 100
+    list_max_show_all = 100
+    date_hierarchy = 'publish_at'
+    search_fields = ['name']
+    prepopulated_fields = {'slug':('name',)}
 
-
-admin.site.register(Category, CategoryAdmin)
-
-
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug', 'price', 'stock',
-                    'available', 'created', 'updated']
-    list_filter = ['available', 'created', 'updated']
-    list_editable = ['price', 'stock', 'available']
-    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name','slug','category','publish_at','updated_at','stock','price']
+    list_display_links = ['name']
+    list_editable = ['price','stock']
+    list_filter = ['name','category','publish_at']
+    list_per_page = 100
+    list_max_show_all = 100
+    date_hierarchy = 'publish_at'
+    search_fields = ['name']
+    prepopulated_fields = {'slug':('name',)}
+    list_select_related = ['category']
 
-
-admin.site.register(Product, ProductAdmin)
+    def category_name(self,obj):
+        return obj.category.name
