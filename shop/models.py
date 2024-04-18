f07369533726c73451de19024769398c57b4c951
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-04-19 01:00:29 +0330

    Update models.py

--- shop/models.py
+++ shop/models.py
@@ -1,49 +1,43 @@
-from django.core.urlresolvers import reverse
 from django.db import models
-
+from django.utils import timezone
+from django.urls import reverse
+# Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=200,
-                            db_index=True)
-    slug = models.SlugField(max_length=200,
-                            db_index=True,
-                            unique=True)
+    name = models.CharField(max_length=250,db_index=True)
+    slug = models.SlugField(db_index=True)
+    publish_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
-        ordering = ('name',)
+        ordering = ('publish_at',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return self.name
+        return f'your category name {self.name} and published at {self.publish_at}'
 
     def get_absolute_url(self):
-        return reverse('shop:product_list_by_category',
-                       args=[self.slug])
-
+        return reverse('shop:products_by_category',args=[self.slug])
 
 class Product(models.Model):
-    category = models.ForeignKey(Category,
-                                 related_name='products')
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
-                              blank=True)
-    description = models.TextField(blank=True)
-    # 台灣價錢都是整數，所以可以設定 decimal_places=0
-    price = models.DecimalField(max_digits=10, decimal_places=0)
+    name = models.CharField(max_length=255,db_index=True)
+    slug = models.SlugField(db_index=True)
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    publish_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True,auto_now_add=False)
     stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
 
     class Meta:
-        ordering = ('name',)
-        index_together = (('id', 'slug'),)
+        ordering = ('publish_at',)
+        index_together = (('id','slug'),)
+        verbose_name = 'product'
+        verbose_name_plural= 'products'
 
     def __str__(self):
-        return self.name
+        return f'product name is {self.name} and published to {self.publish_at}'
 
     def get_absolute_url(self):
-        return reverse('shop:product_detail',
-                       args=[self.id, self.slug])
+        return reverse('shop:product_detail',args=[self.id,self.slug])
